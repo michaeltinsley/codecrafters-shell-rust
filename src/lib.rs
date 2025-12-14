538c791e2315d51b36c86943ec1130ccf991a6b5
Michael Tinsley <27818609+michaeltinsley@users.noreply.github.com>
2025-12-14 17:17:48 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::os::unix::fs::PermissionsExt;
+use std::path::PathBuf;
 use std::str::FromStr;
 
 pub enum ShellStatus {
@@ -45,15 +48,42 @@ pub fn echo_cmd(args: Vec<&str>) {
 }
 
 pub fn type_cmd(args: Vec<&str>) {
-    let builtins = ["echo", "type", "exit"];
-
-    if let Some(&first) = args.first() {
-        if builtins.contains(&first) {
-            println!("{} is a shell builtin", first);
-        } else {
-            println!("{}: not found", first);
+    let command = match args.first() {
+        Some(cmd) => cmd,
+        None => {
+            return;
         }
-    } else {
-        println!("type: missing argument");
+    };
+    // 1. Check if it's a builtin
+    if Builtin::from_str(command).is_ok() {
+        println!("{} is a shell builtin", command);
+        return;
     }
+
+    // 2. External command check
+    match get_executable_path(command) {
+        Some(path) => println!("{} is {}", command, path.display()),
+        None => println!("{}: not found", command),
+    }
+}
+
+fn get_executable_path(command: &str) -> Option<PathBuf> {
+    // Get the PATH environment variable
+    let path_var = env::var("PATH").ok()?;
+
+    // split_paths handles the delimiters for us (: on Unix, ; on Windows)
+    for path in env::split_paths(&path_var) {
+        let full_path = path.join(command);
+
+        if full_path.is_file()
+            && let Ok(metadata) = full_path.metadata()
+        {
+            let mode = metadata.permissions().mode();
+            // 0o111 is the octal mask for execute permissions
+            if mode & 0o111 != 0 {
+                return Some(full_path);
+            }
+        }
+    }
+    None
 }
